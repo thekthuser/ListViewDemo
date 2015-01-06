5b63ba792a4a5ffb35d8e629d400fdeeb71467d1
thekthuser <thekthuser@thekthuser.com>
2015-01-06 16:15:08 -0500

    simple static list activity

--- src/com/thekthuser/listviewdemo/MainActivity.java
+++ src/com/thekthuser/listviewdemo/MainActivity.java
@@ -1,15 +1,24 @@
 package com.thekthuser.listviewdemo;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity
+public class MainActivity extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+
+        String[] items = {
+            "item1", 
+            "item2", 
+            "item3"
+        };
+
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.title, items));
     }
 }
