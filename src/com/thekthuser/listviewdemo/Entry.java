70fe5a08119baf7eab43f35f9f5b53dd10d4d652
thekthuser <thekthuser@thekthuser.com>
2015-01-18 13:41:12 -0500

    new constructor for Entry

--- src/com/thekthuser/listviewdemo/Entry.java
+++ src/com/thekthuser/listviewdemo/Entry.java
@@ -31,4 +31,32 @@ public class Entry {
         this.entryId = entryId;
         this.name = name;
     }
+
+    public Entry(int entryId, String name, String summary, String price_amount, String price_currency, String content_type_term, String content_type_label, String rights, String title, String link_rel, String link_type, String link_href, String id_label, String id_id, String id_bundleId, String artist_label, String artist_href, String category_id, String category_term, String category_scheme, String category_label, String release_date, String release_date_human) {
+        this.entryId = entryId;
+        this.name = name;
+        this.summary = summary;
+        this.price_amount = price_amount;
+        this.price_currency = price_currency;
+        this.content_type_term = content_type_term;
+        this.content_type_label = content_type_label;
+        this.rights = rights;
+        this.title = title;
+        this.link_rel = link_rel;
+        this.link_type = link_type;
+        this.link_href = link_href;
+        this.id_label = id_label;
+        this.id_id = id_id;
+        this.id_bundleId = id_bundleId;
+        this.artist_label = artist_label;
+        this.artist_href = artist_href;
+        this.category_id = category_id;
+        this.category_term = category_term;
+        this.category_scheme = category_scheme;
+        this.category_label = category_label;
+        this.release_date = release_date;
+        this.release_date_human = release_date_human;
+
+    }
+
 }
