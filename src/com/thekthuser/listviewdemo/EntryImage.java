bced7565b77637b8810b784b444812b94f42e073
thekthuser <thekthuser@thekthuser.com>
2015-01-20 16:41:28 -0500

    created EntryImage, cleaned up MainActivity

---
+++ src/com/thekthuser/listviewdemo/EntryImage.java
@@ -0,0 +1,17 @@
+package com.thekthuser.listviewdemo;
+
+public class EntryImage {
+    public int imageId;
+    public int entryid_id;
+    public String url;
+    public int height;
+
+
+    public EntryImage(imageId, entryid_id, url, height) {
+        this.imageId = imageId;
+        this.entryid_id = entryid_id;
+        this.url = url;
+        this.height = height;
+    }
+
+}
