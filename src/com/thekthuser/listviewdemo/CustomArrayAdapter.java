1d1c9374e1f5346c81cf83196e4e02f9359fcca8
thekthuser <thekthuser@thekthuser.com>
2015-01-20 19:52:11 -0500

    MainActivity list now displays the right images

--- src/com/thekthuser/listviewdemo/CustomArrayAdapter.java
+++ src/com/thekthuser/listviewdemo/CustomArrayAdapter.java
@@ -30,9 +30,11 @@ public class CustomArrayAdapter extends ArrayAdapter<Entry> {
         }
 
         Entry entry = data[position];
+        EntryImage[] images = entry.images;
 
         ImageView list_image = (ImageView) convertView.findViewById(R.id.list_image);
-        new ImageDownloader(list_image).execute("http://a205.phobos.apple.com/us/r30/Purple3/v4/72/4d/ae/724daeb9-07ba-2898-b924-2be4983131c5/mzl.tvvvvggs.53x53-50.png");
+        //assuming images are always in ascending height here
+        new ImageDownloader(list_image).execute(images[0].url);
 
         TextView title = (TextView) convertView.findViewById(R.id.title);
         title.setText(entry.name);
