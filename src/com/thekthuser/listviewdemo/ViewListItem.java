f569f578806120094a6ca14fe7257f93781b326c
thekthuser <thekthuser@thekthuser.com>
2015-01-27 16:10:48 -0500

    ViewListItem displays full information

--- src/com/thekthuser/listviewdemo/ViewListItem.java
+++ src/com/thekthuser/listviewdemo/ViewListItem.java
@@ -12,7 +12,10 @@ public class ViewListItem extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.view_list_item);
 
-        //TextView id = (TextView) findViewById(R.id.display_id);
+        TextView title = (TextView) findViewById(R.id.display_title);
+        TextView summary = (TextView) findViewById(R.id.display_summary);
+        TextView price = (TextView) findViewById(R.id.display_price);
+        TextView date = (TextView) findViewById(R.id.display_date);
         ImageView image = (ImageView) findViewById(R.id.view_list_image);
 
         Bundle b = getIntent().getExtras();
@@ -20,6 +23,10 @@ public class ViewListItem extends Activity {
         String image_url = entry.images[2].url; //assuming the third image exists and is largest
 
 
+        title.setText(entry.title);
+        summary.setText(entry.summary);
+        price.setText("$" + entry.price_amount);
+        date.setText("Released on " + entry.release_date_human);
         new ImageDownloader(image).execute(image_url);
     }
 }
